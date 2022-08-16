a1e83d36ca16ce538f5a272388afe48b754283d0
milkywayMW1998 <trinhquang16111998@gmail.com>
2022-08-16 14:09:53 +0700

    update

--- BTVN-07-JS/js/BTVN6-1.js
+++ BTVN-07-JS/js/BTVN6-1.js
@@ -1,3 +1,28 @@
+// - Var : được xác định có phạm vi toàn cục (global) hoặc phạm vi cục bộ/ hàm nếu khai báo trong hàm.
+// Phạm vi toàn cục nếu khai báo biến nằm ngoài khai báo một hàm. Nghĩa một biến khi khi báo bên ngoài khối khai báo hàm có thể dùng trong toàn bộ window. Biến được khai báo trong hàm thì chỉ có thể truy cập ở trong khối khai báo của hàm.
+
+// - Let : Let được định phạm vi trong khối mã (Block-scoped)
+// Một khối là một đoạn mã được bao bởi cặp dấu mở ngoặc nhọn và đóng ngoặc nhọn { }. Mọi lệnh trong cặp dấu là một khối mã (Block)
+// Biến khai báo với let trong khối mã chỉ có thể dùng trong khối mã đó
+// - Let có thể được cập nhật nhưng không được khai báo lại
+// Giống với khai báo dùng var, biến khai báo với let có thể được cập nhật trong phạm vi tồn tại. Nhưng không giống với var, biến let không thể khai báo lại trong phạm vi tồn tại
+// thực tế, dùng let tốt hơn var. Khi dùng let, không bận tâm biến có dùng trùng tên với biến khai báo trước đó ở phạm vi tồn tại khác
+
+// - Const : Biến được khai báo với const chứa giá trị hằng (Giá trị không thể thay đổi trong khi thực hiện). Khai báo const chia sẽ tính tương đồng với khai báo let.
+// Khai báo const được định phạm vi theo khối mã (Block-scoped)
+// Giống với khai báo let, khai báo biến const chỉ được truy cập trong khối mã biến được khai báo
+// Const không thể được cập nhật và khai báo lại
+// Không thể cập nhật và khai báo lại nghĩa là giá trị của biên không thể được thay đổi và không thể khai báo biến có trùng tên trong cùng phạm vi tồn tại
+// Giống với let, khai báo const được dịch chuyển lên đầu của đoạn mã nhưng không được khởi gán giá trị.
+
+
+//Giữa var, let, const có thể được tóm lược như sau:
+// + Khai báo var được định phạm vi toàn cục (global) hay hàm (function) trong khi let và const được định phạm vi là khối mã (block)
+// + Biến var có thể được cập nhật và khai báo lại trong phạm vi tồn tại; biến let có thể được cập nhật nhưng không thể khai báo lại; biến const không thể cập nhật nhưng không thể khai báo lại.
+// + Khai báo của var, let, const đều được dịch chuyển lên đầu của phạm vi. Nhưng trong khi biến var được khởi gán giá trị với undefined, biến let và const không được khởi gán giá trị.
+// + Trong khi var và let có thể được khai báo không khởi gán giá trị, const phải khởi gán giá trị khi khai báo.
+
+
 // Dùng charAt in ra ký tự "y" trong chuỗi sau:
 // Phương thức string.charAt() có chức năng tìm kiếm kí tự ở một vị trí nào đó trong chuỗi, vị trí tìm kiếm đó sẽ do người dùng xác định.
 let greeting = "Hello. My name is John";
