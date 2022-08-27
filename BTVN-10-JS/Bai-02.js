3872fb380fedf4faba20f53992ce52bdfbd022a9
milkywayMW1998 <trinhquang16111998@gmail.com>
2022-08-27 17:25:08 +0700

    commit update Bai 10

--- BTVN-10-JS/Bai-02.js
+++ BTVN-10-JS/Bai-02.js
@@ -1,24 +1,25 @@
 // B1: Use for...in loop and recursive to get all key of object
 
-const user = {
-  name: "hieu",
-  age: 18,
-  appearence: {
-    hairColor: "black",
-    skinColor: "yeallow",
-    height: 1700,
-    weight: 68,
-  },
-};
 
-function recursive(Object) {
-  for (key in Object) {
-    if (typeof key !== "object") {
-      console.log(key);
-    } else {
-      recursive(key);
+const user = {
+    name: "hieu",
+    age: 18,
+    appearence: {
+      hairColor: "black",
+      skinColor: "yeallow",
+      height: 1700,
+      weight: 68,
+    },
+  };
+  
+  function recursive(target) {
+    for (const key in target) {
+      if (key !== 'e' && typeof target[key] === 'object') {
+        recursive(target[key]);
+      } else {
+        console.log(key, target[key]);
+      }
     }
   }
-}
-
-recursive(user);
+  
+  recursive(user);
